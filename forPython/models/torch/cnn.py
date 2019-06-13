9c24029168b8979cb17f42bc44764301683c3caa
Hirotaka Kawashima <k.hirotaka@me.com>
2019-06-14 01:10:51 +0900

    create model zoo

---
+++ forPython/models/torch/cnn.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self, n_features, mid_size, n_outputs):
+        super(SimpleCNN, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(n_features, 64, 3),
+            nn.ReLU(),
+            nn.Conv1d(64, 128, 3),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            Flatten()
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(mid_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_outputs),
+        )
+
+    def forward(self, x):
+        """
+        :type x: torch.Tensor
+        """
+        x = x.transpose(1, 2)
+        outputs = self.conv(x)
+        outputs = self.fc(outputs)
+        return outputs
